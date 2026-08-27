668f5786c3b34b09483addf76e22ffc9d98c9dbd
Vivek Singh Chauhan <vschauhan236679@gmail.com>
2026-08-27 22:31:53 +0530

    created User and Meeting Schemas

---
+++ backend/src/models/MeetingModel.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+
+const meetingModel = new Schema(
+    {
+        user_id: {
+            type: String
+        },
+        meeting_code: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: Date,
+            default: Date.now, 
+            required: true
+        }
+    }
+)
+
+const Meeting = mongoose.model("Meeting", meetingSchema);
+
+export { Meeting };
